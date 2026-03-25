35d45ede33911785a155298b11d95d956fde5525
Tristan <tmd526@gmail.com>
2026-03-25 14:02:53 +0000

    fixed some formatting created /test folder and finished parentnode.py

---
+++ src/parentnode.py
@@ -0,0 +1,24 @@
+try:
+    from .htmlnode import HTMLNode
+except Exception:
+    from htmlnode import HTMLNode
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+    
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("invalid HTML: no children")
+        ret_string = f"<{self.tag}>"
+        for child in self.children:
+            # child objects expose a to_html() method
+            ret_string += child.to_html()
+        ret_string += f"</{self.tag}>"
+        return ret_string
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+        
